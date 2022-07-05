23d8d45fb766068b9394e6efa2cb9668647f9291
Israeltheminer <israeliyanda5@gmail.com>
2022-07-05 12:17:31 +0100

    Responsive update for various devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
    content: ["./views/**/*.{ejs, html,js}"],
    theme: {
+    screens: {
+      '2xl': {'max': '2000px'},
+      'xl': {'max': '1200px'},
+      'lg': {'max': '1000px'},
+      'md': {'max': '750px'},
+      'sm': {'max': '450px'},
+      'xs': {'max': '375px'},
+    },
      extend: {},
    },
    plugins: [],
